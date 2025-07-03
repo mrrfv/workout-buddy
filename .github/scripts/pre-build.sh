7cb7866b04c7c97155eabcbe1f2bd6d9244488ad
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2025-07-03 20:53:22 +0200

    Install Clang in CI

--- .github/scripts/pre-build.sh
+++ .github/scripts/pre-build.sh
@@ -6,7 +6,7 @@ if command -v apk >/dev/null 2>&1; then
   # Alpine Linux
   echo "Detected Alpine Linux - installing via apk"
   apk update
-  apk add --no-cache pkgconf wayland-dev pipewire-dev
+  apk add --no-cache pkgconf wayland-dev pipewire-dev clang-dev
 
 elif command -v apt-get >/dev/null 2>&1; then
   # Debian / Ubuntu
@@ -15,7 +15,8 @@ elif command -v apt-get >/dev/null 2>&1; then
   DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
     pkg-config \
     libwayland-dev \
-    libpipewire-0.3-dev
+    libpipewire-0.3-dev \
+    clang libclang-dev
 else
   echo >&2 "No supported package manager found (need apk or apt-get)."
 fi
