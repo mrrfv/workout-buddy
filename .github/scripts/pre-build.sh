45df379fbf58a1e61cbed8341291d49994899361
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2025-07-04 23:07:19 +0200

    Install ALSA dependencies on Linux CI

--- .github/scripts/pre-build.sh
+++ .github/scripts/pre-build.sh
@@ -6,7 +6,7 @@ if command -v apk >/dev/null 2>&1; then
   # Alpine Linux
   echo "Detected Alpine Linux - installing via apk"
   apk update
-  apk add --no-cache pkgconf wayland-dev pipewire-dev clang-dev
+  apk add --no-cache pkgconf wayland-dev pipewire-dev clang-dev alsa-lib-dev
 
 elif command -v apt-get >/dev/null 2>&1; then
   # Debian / Ubuntu
@@ -16,7 +16,8 @@ elif command -v apt-get >/dev/null 2>&1; then
     pkg-config \
     libwayland-dev \
     libpipewire-0.3-dev \
-    clang libclang-dev
+    clang libclang-dev \
+    libasound2-dev
 else
   echo >&2 "No supported package manager found (need apk or apt-get)."
 fi
